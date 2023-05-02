e1b89a3bfaf0afb78384b390ada48330e9d0cb7c
Ömer Faruk Şahan <sahan.faruk.omer@gmail.com>
2023-05-02 17:57:28 +0300

    refactor coding

--- src/components/RightContainer.jsx
+++ src/components/RightContainer.jsx
@@ -74,10 +74,10 @@ margin-bottom: 2.5em;
 border-right: 3px solid black;
 &:nth-child(4) {
     border: none;
-  }
+}
 &:last-child {
     border: none;
-  }
+}
 &:hover {
   background-color: rgba(255, 255, 255, 0.37);
   transition: all 0.25s;
